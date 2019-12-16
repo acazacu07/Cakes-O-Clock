65cd9a2c5690c17cd5cf1659c33cfeba92d52a5b
EC2 Default User <ec2-user@ip-172-31-18-10.ec2.internal>
2019-12-16 17:02:18 +0000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,31 +27,64 @@ Item.create!(image_url: 'tiramisu_cake.jpg',
 title:'Tiramisu Cake',
 category:'Cakes',
 description:'A popular coffee-flavoured Italian dessert',
-price:'30')
+price:'20')
 # . . .
 Item.create!(image_url: 'choco_cake.jpg',
 title:'Chocolate Cake',
 category:'Cakes',
 description:'A rich, dense, moist chocolate cake',
-price:'30')
+price:'25')
 # . . .
-Item.create!(image_url: 'choco_cake.jpg',
+Item.create!(image_url: 'cheese_cake.jpg',
 title:'Cheese Cake',
 category:'Cakes',
 description:'Can be prepared in flavors such as strawberry or toffee.',
 price:'25')
 # . . .
+Item.create!(image_url: 'lemon_cake.jpg',
+title:'Lemon Cake',
+category:'Cakes',
+description:'A combination of flour, sugar, eggs, lemon zest and butter.',
+price:'25')
+# . . 
+Item.create!(image_url: 'berry_cake.jpg',
+title:'Berrys Cake',
+category:'Cakes',
+description:'A combination of flour, sugar, eggs, lemon zest and butter.',
+price:'20')
+# . . .
 Item.create!(image_url: 'brownies_prod.jpg',
-title:'Brownies',
+title:'Blondie Brownies',
+category:'Brownies',
+description:'Blondies are really butterscotch bars, made with brown sugar, butter, and eggs and usually nuts, but no chocolate.',
+price:'15')
+# . . .
+Item.create!(image_url: 'fudgy_brownies.jpg',
+title:'Fudgy Brownies',
 category:'Brownies',
 description:'A flat American square cookie',
-price:'10')
+price:'15')
 # . . .
-Item.create!(image_url: 'cupcakes_product.jpg',
-title:'Cupcakes',
+Item.create!(image_url: 'almond_brownies.jpg',
+title:'Almond Brownies',
+category:'Brownies',
+description:'A flat American square cookie',
+price:'15')
+# . . .
+Item.create!(image_url: 'fruit-merenge_cupcake.jpg',
+title:'Fruit-Merengue Cupcakes',
 category:'Cupcakes',
 description:'A small cake designed to serve one person',
-price:'10')
+price:'15')
+# . . .
+Item.create!(image_url: 'cookies_product.jpg',
+title:'Cookies',
+category:'Cookies',
+description:'American Cookies',
+price:'15')
+
+
+
 
 User.create!(FirstName:'admin',
 LastName:'X',
